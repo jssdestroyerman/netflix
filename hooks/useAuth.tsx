156403866736ee5c616b76e60e00ba83ab1d690d
jssdestroyerman <jesuisdestroyerman@proton.me>
2023-02-03 11:15:00 +0100

    router push changed

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -63,7 +63,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
         await createUserWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
                 setUser(userCredential.user);
-                router.push("/signup/account");
+                router.push("/");
                 setLoading(false);
             })
             .catch((error) => alert(error.message))
