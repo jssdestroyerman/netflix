2fe97b22b70eea533f8945d60c5234eddd0cb1d1
jssdestroyerman <jesuisdestroyerman@proton.me>
2023-02-04 17:51:27 +0100

    working on

--- components/Membership.tsx
+++ components/Membership.tsx
@@ -1,5 +1,39 @@
+import useAuth from "@/hooks/useAuth";
+import useSubscription from "@/hooks/useSubscription";
+import { goToBillingPortal } from "@/lib/stripe";
+import { useState } from "react";
+import Loader from "./Loader";
+
 function Membership() {
-    return <div>membership</div>;
+    const { user } = useAuth();
+    const subscription = useSubscription(user);
+    const [isBillingLoading, setBillingLoading] = useState(false);
+
+    function manageSubscription() {
+        if (subscription) {
+            setBillingLoading(true);
+            goToBillingPortal();
+        }
+    }
+
+    return (
+        <div className="mt-6 grid grid-cols-1 gap-x-4 border px-4 md:grid-cols-4 md:border-x-0 md:border-t md:border-b-0 md:px-0">
+            <div className=" space-y-2 py-4">
+                <h4 className="text-lg text-[gray]">Membership & Billing</h4>
+                <button
+                    disabled={isBillingLoading || !subscription}
+                    className="h-10 w-3/5 whitespace-nowrap bg-gray-300 py-2 text-sm font-medium text-black shadow-md hover:bg-gray-200 md:w-4/5"
+                    onClick={manageSubscription}
+                >
+                    {isBillingLoading ? (
+                        <Loader color="dark:fill-[#e50914]" />
+                    ) : (
+                        "Cancel Membership"
+                    )}
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default Membership;
