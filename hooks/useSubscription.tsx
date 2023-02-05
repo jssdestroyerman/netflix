8793f967915ed5b46995702458db63e93829b082
jssdestroyerman <jesuisdestroyerman@proton.me>
2023-02-05 19:41:38 +0100

    if added

--- hooks/useSubscription.tsx
+++ hooks/useSubscription.tsx
@@ -10,6 +10,8 @@ function useSubscription(user: User | null) {
     const [subscription, setSubscription] = useState<Subscription | null>(null);
 
     useEffect(() => {
+        if (!user) return;
+
         if (user) {
             onCurrentUserSubscriptionUpdate(payments, (snapshot) => {
                 setSubscription(
